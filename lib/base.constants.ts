039ff078297934f1d38526c22d00f6d187330067
dominikdeimel <info@dominikdeimel.com>
2024-01-20 19:31:43 +0100

    chore(homepage): added list of tools for the homepage

--- lib/base.constants.ts
+++ lib/base.constants.ts
@@ -1,4 +1,10 @@
-export const MENU_ITEMS = [
+type MenuItem = {
+  link: string;
+  name: string;
+  description?: string;
+};
+
+export const MENU_ITEMS: MenuItem[] = [
   {
     link: '/',
     name: 'Home',
@@ -6,5 +12,7 @@ export const MENU_ITEMS = [
   {
     link: '/password-generator',
     name: 'Password Generator',
+    description:
+      'Ein Passwort-Generator ist ein Tool, das starke und sichere Passwörter erstellt, die schwer zu knacken sind. Es generiert zufällige Passwörter, die aus Buchstaben, Zahlen und Sonderzeichen bestehen, um eine hohe Sicherheit zu gewährleisten. Dieses Tool ist besonders nützlich, um einzigartige Passwörter für jede Ihrer Online-Konten zu erstellen und so Ihre digitale Sicherheit zu erhöhen.',
   },
 ];
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { Card, CardContent, CardFooter, CardHeader } from '~/components/ui/card';
+import { MENU_ITEMS } from '~/lib/base.constants';
+import { Button } from '~/components/ui/button';
+import { NuxtLink } from '#components';
+
+const menuItems = computed(() => MENU_ITEMS.filter(item => item.link !== '/'));
+</script>
+
 <template>
-  Das ist die Startseite
+  <h1 class="mb-4 text-2xl lg:mb-8 lg:text-4xl">Alle Tools im Überblick</h1>
+  <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <Card v-for="page in menuItems">
+      <CardHeader>
+        <h3 class="text-2xl font-semibold leading-none tracking-tighter">{{ page.name }}</h3>
+      </CardHeader>
+      <CardContent>
+        <p v-if="page?.description">{{ page.description }}</p>
+      </CardContent>
+      <CardFooter>
+        <Button size="sm" class="w-full" :as="NuxtLink" :to="page.link">Zum Tool</Button>
+      </CardFooter>
+    </Card>
+  </div>
 </template>
