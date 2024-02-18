5a0f84fe2a4dfcec79226a5b831811c9c451e006
dominikdeimel <info@dominikdeimel.com>
2024-02-18 19:37:10 +0100

    chore: removed unused exports

--- lib/password-generator/utils.ts
+++ lib/password-generator/utils.ts
@@ -1,5 +1,5 @@
-import { isEmpty, omitBy, random, sample } from 'lodash-es'
-import type { GenerationProperties, RandomFunc } from '~/types/password-generator.model'
+import { isEmpty, omitBy, random, sample } from 'lodash-es';
+import type { GenerationProperties, RandomFunc } from '~/types/password-generator.model';
 
 /**
  * Create an array and fill it with a range of numbers
@@ -7,16 +7,16 @@ import type { GenerationProperties, RandomFunc } from '~/types/password-generato
 const range = (start: number, end: number): number[] => {
   return Array(end - start + 1)
     .fill(null)
-    .map((_, idx) => start + idx)
-}
+    .map((_, idx) => start + idx);
+};
 
 /**
  * Create a string of special Chars
  */
 const createSymbolString = (): string[] => {
-  const asciiCharsRanges: number[] = [...range(33, 47), ...range(58, 64), ...range(91, 96), ...range(123, 126)]
-  return asciiCharsRanges.map(char => String.fromCharCode(char))
-}
+  const asciiCharsRanges: number[] = [...range(33, 47), ...range(58, 64), ...range(91, 96), ...range(123, 126)];
+  return asciiCharsRanges.map(char => String.fromCharCode(char));
+};
 
 /**
  * Generates Random Char
@@ -26,7 +26,7 @@ const getRandomChar = (start: number, end: number): string => String.fromCharCod
 /**
  * Generates Random Symbol
  */
-const getRandomSymbol = (symbols: string [] = createSymbolString()): string => symbols[random(0, symbols.length - 1)]
+const getRandomSymbol = (symbols: string[] = createSymbolString()): string => symbols[random(0, symbols.length - 1)];
 
 /**
  * Generate Password
@@ -36,21 +36,19 @@ const randomFunc: RandomFunc = {
   lower: () => getRandomChar(97, 122),
   upper: () => getRandomChar(65, 90),
   number: () => getRandomChar(48, 57),
-  symbol: getRandomSymbol
-}
+  symbol: getRandomSymbol,
+};
 
 const generatePassword = ({ length, ...args }: GenerationProperties): string => {
-  type FilteredTypes = Omit<GenerationProperties, 'length'>
-  const filteredTypes = omitBy<FilteredTypes>(args, (value) => !Boolean(value))
-  if (isEmpty(filteredTypes)) return ''
-  const typesArray = Object.entries(filteredTypes as FilteredTypes)
+  type FilteredTypes = Omit<GenerationProperties, 'length'>;
+  const filteredTypes = omitBy<FilteredTypes>(args, value => !Boolean(value));
+  if (isEmpty(filteredTypes)) return '';
+  const typesArray = Object.entries(filteredTypes as FilteredTypes);
 
   return Array.from({ length }, () => {
-    const [type] = sample(typesArray) as [keyof FilteredTypes, boolean]
-    return randomFunc[type as keyof RandomFunc]()
-  }).join('')
-}
+    const [type] = sample(typesArray) as [keyof FilteredTypes, boolean];
+    return randomFunc[type as keyof RandomFunc]();
+  }).join('');
+};
 
-export {
-   getRandomSymbol, generatePassword
-}
+export { generatePassword };
